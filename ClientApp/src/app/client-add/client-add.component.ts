e3c64f1db5b5f41f2991fe90dbd3bacbd5b05992
Shavy <shavy@Shavys-MacBook-Pro.local>
2021-07-12 03:40:37 +1000

    Comma error removed

--- ClientApp/src/app/client-add/client-add.component.ts
+++ ClientApp/src/app/client-add/client-add.component.ts
@@ -8,32 +8,36 @@ import { FormControl } from '@angular/forms';
 })
 export class ClientAddComponent implements OnInit {
 
-  frm = new FormGroup({
-    name: new FormControl('clientname'),
-    address: new FormControl('clientaddress')
-    address2: new FormControl('clientaddress2')
-    city: new FormControl('clientcity')
-    state: new FormControl('clientstate')
-    pincode: new FormControl('clientpincode')
-    country: new FormControl('clientcountry')
-  });
-  constructor(private service: ClientsService) { }
-  ngOnInit() { }
-  submitPost(e) {
-    e.preventDefault();
-    let params = {
-      name: this.frm.get('name').value,
-      address: this.frm.get('address').value,
-      address2: this.frm.get('address2').value,
-      city: this.frm.get('city').value,
-      state: this.frm.get('state').value,
-      pincode: this.frm.get('pincode').value,
-      country: this.frm.get('country').value,
-    };
-    this.service.postData(params).subscribe(response => {
-      console.log(response);
-    });
+  constructor() { }
+  ngOnInit() {
   }
+
+  //frm = new FormGroup({
+  //  name: new FormControl('clientname'),
+  //  address: new FormControl('clientaddress'),
+  //  address2: new FormControl('clientaddress2'),
+  //  city: new FormControl('clientcity'),
+  //  state: new FormControl('clientstate'),
+  //  pincode: new FormControl('clientpincode'),
+  //  country: new FormControl('clientcountry')
+  //});
+  //constructor(private service: ClientsService) { }
+  //ngOnInit() { }
+  //submitPost(e) {
+  //  e.preventDefault();
+  //  let params = {
+  //    name: this.frm.get('name').value,
+  //    address: this.frm.get('address').value,
+  //    address2: this.frm.get('address2').value,
+  //    city: this.frm.get('city').value,
+  //    state: this.frm.get('state').value,
+  //    pincode: this.frm.get('pincode').value,
+  //    country: this.frm.get('country').value,
+  //  };
+  //  this.service.postData(params).subscribe(response => {
+  //    console.log(response);
+  //  });
+  //}
 
 }
 
